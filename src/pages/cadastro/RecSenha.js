f0f27309ab2afeb4ae741944a6735da2c636c51d
linefersantos <82233560+Guilherme12342br@users.noreply.github.com>
2023-09-18 08:06:46 -0300

    Alterar senha

--- src/pages/cadastro/RecSenha.js
+++ src/pages/cadastro/RecSenha.js
@@ -1,12 +1,34 @@
-import { Text, TouchableOpacity, StyleSheet, View } from 'react-native'
-import ContainerCadastro from '../../components/cadastro/ContainerCadastro';
-import CampoSimples from '../../components/cadastro/CampoSimples';
-import BotaoCadastrar from '../../components/cadastro/BotaoCadastrar';
+import { Text, TouchableOpacity, StyleSheet, View ,Alert} from 'react-native'
+import ContainerCadastro from '../../components/components_cadastro/ContainerCadastro';
+import CampoSimples from '../../components/components_cadastro/CampoSimples';
+import BotaoCadastrar from '../../components/components_cadastro/BotaoCadastrar';
 import { useState } from 'react';
+import axios from 'axios';
 
 const RecSenha = ({ navigation: { navigate } }) => {
 
   const [email, setEmail] = useState('');
+  const [code, setCode] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const generateCode = async () => {
+    try {
+      const response = await axios.post('http://172.18.101.5:3001/generate-code', { email });
+      Alert.alert('Sucesso', 'Código gerado e enviado por email.');
+    } catch (error) {
+      Alert.alert('Erro', 'Ocorreu um erro ao gerar o código.');
+      console.error(error)
+    }
+  };
+
+  const resetPassword = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/reset-password', { email, code, newPassword });
+      Alert.alert('Sucesso', 'Senha alterada com sucesso.');
+    } catch (error) {
+      Alert.alert('Erro', 'Ocorreu um erro ao alterar a senha.');
+    }
+  };
 
   const Enviar = () => {
     navigate("VerCodigo");
@@ -15,8 +37,8 @@ const RecSenha = ({ navigation: { navigate } }) => {
   return (
     <ContainerCadastro titulo="Recuperar conta">
       <View style={styles.container}>
-        <CampoSimples set={text => setEmail(text)} placeholder={"Email"} keyboardType='email-address' />
-        <BotaoCadastrar texto="Enviar código" onPress={Enviar} />
+        <CampoSimples set={setEmail} placeholder={"Email"} keyboardType='email-address' />
+        <BotaoCadastrar texto="Enviar código" onPress={generateCode} />
       </View>
     </ContainerCadastro>
   )
