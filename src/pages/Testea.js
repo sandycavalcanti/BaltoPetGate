0971cba00da549a3266aecb52ed9e4fafa2afa40
Renan Mochizuki <renanmochizuki@gmail.com>
2023-09-20 14:48:13 -0300

    Adicionado teste

---
+++ src/pages/Testea.js
@@ -0,0 +1,16 @@
+import { View,Text } from "react-native"
+
+const Teste = () => {
+  return (
+    <View>
+      <Text>TESTE</Text>
+      <Text>TESTE</Text>
+      <Text>TESTE</Text>
+      <Text>TESTE</Text>
+      <Text>TESTE</Text>
+      <Text>TESTE</Text>
+    </View>
+  )
+}
+
+export default Teste
