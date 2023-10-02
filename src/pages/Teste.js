9db52cd4d9b11890ad8b0730be871c805b2c2764
Renan Mochizuki <renanmochizuki@gmail.com>
2023-10-02 07:42:07 -0300

    Testando imagem

--- src/pages/Teste.js
+++ src/pages/Teste.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import { Button, Image, View, TouchableOpacity, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { TouchableOpacity } from 'react-native';
 import axios from 'axios';
-import { urlLocal } from '../constants';
+import { urlAPI, urlLocal } from '../constants';
 
 export default function App() {
   const [image, setImage] = useState(null);
@@ -12,40 +11,62 @@ export default function App() {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
-
-    console.log(result);
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
     }
   };
+  // Função para enviar a imagem
+  const enviarImagem = async () => {
+    try {
+      // Crie um objeto FormData para enviar a imagem
+      const formData = new FormData();
+      formData.append('image', {
+        name: 'image.jpg', // Nome da imagem que será usado no servidor
+        type: 'image/jpeg', // Tipo da imagem (pode variar dependendo do formato)
+        uri: image.uri, // A URI da imagem no React Native
+      });
 
+      // Faça a solicitação POST usando o Axios
+      const response = await axios.put(urlAPI + `/altpessoa/8`, formData);
+
+      // Verifique a resposta do servidor
+      if (response.status === 200) {
+        console.log('Imagem enviada com sucesso');
+      } else {
+        console.error('Erro ao enviar imagem');
+      }
+    } catch (error) {
+      console.error('Erro ao enviar imagem:', error);
+    }
+  };
   const Enviar = async () => {
-    const formData = new global.FormData();
-    formData.append(
-      "image", { name: "image.jpeg", type: "image/jpg", uri: image }
-    )
-    console.log(formData)
-    return fetch(urlLocal + 'upload', {
-      method: "PUT",
-      // Needs this header
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      body: formData
-    }).then(response => {
-      // console.log(response)
-    })
+    if (!image) {
+      return console.error('Nenhuma imagem selecionada.');
+    }
+
+    const formData = new FormData();
+    formData.append('image', image);
+    // formData.append('image', { uri: image, name: 'photo.png', filename: 'imageName.png', type: 'image/png' });
+    // formData.append('Content-Type', 'image/png');
+    // formData.append('TB_PESSOA_UF', 'SP');
+
+    await axios.put(urlLocal + 'altpessoa/8', image)
+      .then(response => {
+        console.log(response.data);
+      }).catch(error => {
+        console.error(error);
+      })
   }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      <Button title="Enviar" onPress={Enviar} />
+      <Button title="Enviar" onPress={enviarImagem} />
     </View>
   );
 }
